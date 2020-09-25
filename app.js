de604a4d6c8d4cab7c00d5e9682464531c49f95d
Henggao Cai <creaturemonster@users.noreply.github.com>
2020-09-25 15:39:56 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,35 +1,33 @@
 const app = Vue.createApp({
   data() {
     return {
-      number:0
+      number: 0
     };
   },
-  computed:{
-    result(){
-      if(this.number < 37){
-        return "Not there yet!";
-      }
-      else if(this.number === 37){
+  computed: {
+    result() {
+      if (this.number < 37) {
+        return 'Not there yet!';
+      } else if (this.number === 37) {
         return this.number;
-      }
-      else{
-        return 'Too Much';
+      } else {
+        return 'Too much!';
       }
     }
   },
-  watch:{
-    result(){
-      console.log("Watcher executing");
+  watch: {
+    result() {
+      console.log('Watcher executing...');
       const that = this;
-      setTimeout=(function() {
-        that.number=0;
+      setTimeout(function() {
+        that.number = 0;
       }, 5000);
-    } 
+    }
   },
   methods: {
-    add(num) {
-      this.counter = this.counter + num;
-    },
+    addNumber(num) {
+      this.number = this.number + num;
+    }
   }
 });
 
