9532c6a23fa67e6b0483b2e8d5e608b8bc47486b
Henggao Cai <creaturemonster@users.noreply.github.com>
2020-09-25 15:35:54 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,20 +2,10 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      number=0,
-      name: '', 
-      fullName:'', 
-      lastName:''
+      number=0
     };
   },
   computed:{
-      fullName(){
-        console.log('Running Again');
-        if(this.name === ''){
-          return '';
-        }
-        return this.name + '' +'Schwarzmuller';
-      },
     result(){
       if(this.number < 37){
         return "Not there yet!";
@@ -35,52 +25,12 @@ const app = Vue.createApp({
       setTimeout=(function() {
         that.number=0;
       }, 5000);
-    }
-  }, 
-    name(value){
-      if(value == ''){
-        this.fullName='';
-      }else{
-      this.fullName=value + ' ' + this.lastName;
-      }
-    }, 
-    counter(value){
-      if(value > 50){
-        setTimeout(function(){
-          this.counter=0;
-        }, 2000);
-      }
-    },
-    lastName(value){
-      if(value == ''){
-        this.fullName='';
-      }else{
-      this.fullName=value + ' ' +  this.lastName;
-      }
-    }
-    
+    } 
   },
   methods: {
-    setName(event, lastName) {
-      this.name = event.target.value + ' ' + lastName;
-    },
     add(num) {
       this.counter = this.counter + num;
     },
-    reduce(num) {
-      this.counter = this.counter - num;
-      // this.counter--;
-    }, 
-    resetInput(){
-      this.name='';
-    }, 
-    outputFullName(){
-      console.log('Running Again');
-      if(this.name === ' '){
-        return ' ';
-      }
-      return this.name + ' ' + 'Schwarzmuller' ;
-    }
   }
 });
 
